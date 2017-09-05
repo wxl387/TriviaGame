a8ecdcf957b9d922933736fb2d5b534c26838de7
Wenbiao Li <lixuyao.tony@gmail.com>
2017-09-05 11:23:35 -0400

    final version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,162 +1,186 @@
 
-var stopwatch = {
-	number: 11,
-	run: function () {
-		stopwatch.number=11;
-		counter = setInterval(stopwatch.increment, 1000);
-	},
-	increment: function() {
-		
-		stopwatch.number--;
-      	$('#show-number').html( "Time left: " + stopwatch.number);
-      	if (stopwatch.number === 0){
-        	stopwatch.stop();
-        	
-        	$('#main').hide().fadeIn("slow");
-			document.getElementById('main').innerHTML="Quiz Complete! You scored " + numCorrect + " out of " + numQuestions + "!";
-			return; // returns false *(there has to be a better way! figure it out.)*
-        }	
-	},
-	stop: function() {
-		clearInterval(counter);
-	}
-};
+var panel = $("#quiz-area");
+var countStartNumber = 30;
 
-stopwatch.run();
-
-var quiz = [ {
-		question: "Who's that Pokemon?",
-		picture: "assets/images/pikachu.png",
-		choices: ['Bulbasaur','Pikachu','Chansey','Diglet'],
-		correct: 1,
-	},
-	{	question: "Who's that Pokemon?",
-		picture: 'assets/images/alakazam.png',
-		choices: ['Mewtwo','Weedle','Metapod','Alakazam'],
-		correct: 3,
-	},
-	{	question: "Who's that Pokemon?",
-		picture: 'assets/images/scyther.png',
-		choices: ['Beedrill','Victreebel','Scyther','Pidgeot'],
-		correct: 2,
-	},
-	{	question: "Who's that Pokemon?",
-		picture: 'assets/images/magmar.png',
-		choices: ['Magmar','Charmeleon','Koffing','Snorlax'],
-		correct: 0,
-	},
-	{	question: "Who's that Pokemon?",
-		picture: 'assets/images/ditto.png',
-		choices: ['Grimer','Ekans','Hitmonlee','Ditto'],
-		correct: 3,
-	},
-	{	question: "Who's that Pokemon?",
-		picture: 'assets/images/zapdos.png',
-		choices: ['Articuno','Moltres','Zapdos','Lugia'],
-		correct: 2,
-	},
-	{	question: "Who's that Pokemon?",
-		picture: 'assets/images/haunter.png',
-		choices: ['Vulpix','Zubat','Haunter','Venonat'],
-		correct: 2,
-	},
-	{	question: "Who's that Pokemon?",
-		picture: 'assets/images/growlithe.png',
-		choices: ['Growlithe','Gyarados','Pinsir','Meowth'],
-		correct: 0,
-	},
-	{	question: "Who's that Pokemon?",
-		picture: 'assets/images/vileplume.png',
-		choices: ['Tentacool','Muk','Shellder','Vileplume'],
-		correct: 3,
-	},
-	{	question: "Who's that Pokemon?",
-		picture: 'assets/images/exeggutor.png',
-		choices: ['Exeguutor','Mr. Mime','Venusaur','Clefable'],
-		correct: 0,
-	},
-	{	question: "Who's that Pokemon?",
-		picture: 'assets/images/mew.png',
-		choices: ['Drowzee','Mew','Kingler','Dratini'],
-		correct: 1,
+// Question set
+var questions = [{
+  question: "What was the first full length CGI movie?",
+  answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+  correctAnswer: "Toy Story",
+  image: "assets/images/toystory.gif"
+}, {
+  question: "Which of these is NOT a name of one of the Spice Girls?",
+  answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
+  correctAnswer: "Fred Spice",
+  image: "assets/images/spicegirls.gif"
+}, {
+  question: "Which NBA team won the most titles in the 90s?",
+  answers: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
+  correctAnswer: "Chicago Bulls",
+  image: "assets/images/bulls.gif"
+}, {
+  question: "Which group released the hit song, 'Smells Like Teen Spirit'?",
+  answers: ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
+  correctAnswer: "Nirvana",
+  image: "assets/images/nirvanabark.gif"
+}, {
+  question: "Which popular Disney movie featured the song, \"Circle of Life\"?",
+  answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
+  correctAnswer: "The Lion King",
+  image: "assets/images/lionking.gif"
+}, {
+  question: "Finish this line from the Fresh Prince of Bel-Air theme song: \"I whistled for a cab and when it came near, the license plate said...\"",
+  answers: ["Dice", "Mirror", "Fresh", "Cab"],
+  correctAnswer: "Fresh",
+  image: "assets/images/fresh.gif"
+}, {
+  question: "What was Doug's best friend's name?",
+  answers: ["Skeeter", "Mark", "Zach", "Cody"],
+  correctAnswer: "Skeeter",
+  image: "assets/images/skeeter.gif"
+}, {
+  question: "What was the name of the principal at Bayside High in Saved By The Bell?",
+  answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
+  correctAnswer: "Mr.Belding",
+  image: "assets/images/belding.gif"
 }];
 
-var numQuestions = quiz.length;
-var numCorrect = 0;
-var counter = 0;
+// Variable to hold our setInterval
+var timer;
 
-var pokePic = $('<img>');
+var game = {
 
-$('input[name="choice"]').hide;
+  questions: questions,
+  currentQuestion: 0,
+  counter: countStartNumber,
+  correct: 0,
+  incorrect: 0,
 
-// display question function
-function nextQuest(){
-	stopwatch.number=11;
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.timeUp();
+    }
+  },
 
-	$('#questions').text(quiz[counter].question);
-	pokePic.attr('src', quiz[counter].picture)
-	$('#pokes').append(pokePic)
-	$('#answer0').text(quiz[counter].choices[0]);
-	$('#answer1').text(quiz[counter].choices[1]);
-	$('#answer2').text(quiz[counter].choices[2]);
-	$('#answer3').text(quiz[counter].choices[3]);
-}
+  loadQuestion: function() {
 
-// client-sided validation
-function validate() {
-	if ($('input').is(':checked')) {
+    timer = setInterval(game.countdown, 1000);
 
-		nextQuest(); // display next question
-	}
-	else {
-		alert("Please make a selection.");
-		counter--;
-	}
-}
+    panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
 
-// display first question
-nextQuest();
+    for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
+      panel.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
+      + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
+    }
+  },
 
-// next button function
-$('#nextBtn').on('click', function() {
-	var answer = ($('input[name="choice"]:checked').val());
+  nextQuestion: function() {
+    game.counter = countStartNumber;
+    $("#counter-number").html(game.counter);
+    game.currentQuestion++;
+    game.loadQuestion();
+  },
 
-	// increment score if answer is correct
-	if (answer == quiz[counter].correct) {
-		numCorrect++;
-	}
+  timeUp: function() {
 
-	counter++;
+    clearInterval(timer);
 
-	// display score screen
-	if (counter >= numQuestions) {
-		$('#main').hide().fadeIn("slow");
-		document.getElementById('main').innerHTML="Quiz Complete! You scored " + numCorrect + " out of " + numQuestions + "!";
-		return; // returns false *(there has to be a better way! figure it out.)*
-	}
+    $("#counter-number").html(game.counter);
 
-	validate();
+    panel.html("<h2>Out of Time!</h2>");
+    panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
+    panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
 
-	// fade in new question
-	$('.card').hide().fadeIn("slow");
-	
-	// clear previous selection
-	$('input[name="choice"]').prop('checked', false);
+    if (game.currentQuestion === questions.length - 1) {
+      setTimeout(game.results, 3 * 1000);
+    }
+    else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+
+  results: function() {
+
+    clearInterval(timer);
+
+    panel.html("<h2>All done, heres how you did!</h2>");
+
+    $("#counter-number").html(game.counter);
+
+    panel.append("<h3>Correct Answers: " + game.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (game.incorrect + game.correct)) + "</h3>");
+    panel.append("<br><button id='start-over'>Start Over?</button>");
+  },
+
+  clicked: function(e) {
+    clearInterval(timer);
+    if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+      this.answeredCorrectly();
+    }
+    else {
+      this.answeredIncorrectly();
+    }
+  },
+
+  answeredIncorrectly: function() {
+
+    game.incorrect++;
+
+    clearInterval(timer);
+
+    panel.html("<h2>Nope!</h2>");
+    panel.append("<h3>The Correct Answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
+    panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+
+    if (game.currentQuestion === questions.length - 1) {
+      setTimeout(game.results, 3 * 1000);
+    }
+    else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+
+  answeredCorrectly: function() {
+
+    clearInterval(timer);
+
+    game.correct++;
+
+    panel.html("<h2>Correct!</h2>");
+    panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+
+    if (game.currentQuestion === questions.length - 1) {
+      setTimeout(game.results, 3 * 1000);
+    }
+    else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+
+  reset: function() {
+    this.currentQuestion = 0;
+    this.counter = countStartNumber;
+    this.correct = 0;
+    this.incorrect = 0;
+    this.loadQuestion();
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start-over", function() {
+  game.reset();
 });
 
-// back button function
-$('#backBtn').on('click', function() {
-	if (quiz[counter] == 0) {
-		$('.card').hide().fadeIn("slow");
-
-	} else {
-		// fade out current question and fade in previous question
-		$('.card').hide().fadeIn("slow");
-		numCorrect--;
-		counter--;
-	}
-	
-	// display previous question
-	nextQuest();	
+$(document).on("click", ".answer-button", function(e) {
+  game.clicked(e);
 });
 
+$(document).on("click", "#start", function() {
+  $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+  game.loadQuestion();
+});
